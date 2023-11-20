d713bd71c72b4a40abe3524f8189e8de2016938d
iontab <iontabuncic96@gmail.com>
2023-11-20 12:57:59 +0100

    cinicial

---
+++ CharAtExample/src/example/CharAtExample.java
@@ -0,0 +1,18 @@
+package example;
+
+public class CharAtExample {
+
+	public static void main(String[] args) {
+		
+		String name="JavaMethods";
+		
+		//charAt() method returns a char value at the given index number.
+		char ch=name.charAt(3);
+		
+		System.out.println(ch);
+		
+		
+
+	}
+
+}
